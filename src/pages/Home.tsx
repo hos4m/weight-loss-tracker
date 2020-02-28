d03e1777598923fedd7264e93f195be487c4d221
Hossam Mourad <ihos4m@gmail.com>
2020-02-27 19:02:33 -0500

    init commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from "@ionic/react";
+import React from "react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Weight Loss Tracker</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
