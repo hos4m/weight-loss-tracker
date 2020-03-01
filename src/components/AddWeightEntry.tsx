a7017166c7dfa0f5990b8ab4f1d9a8e2abc9657b
Hossam Mourad <ihos4m@gmail.com>
2020-02-29 23:20:21 -0500

    add fields for the new weight entry sectio

--- src/components/AddWeightEntry.tsx
+++ src/components/AddWeightEntry.tsx
@@ -1,5 +1,45 @@
 import React, { FC } from "react";
+import {
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonText,
+  IonDatetime,
+  IonChip,
+  IonIcon,
+  IonRow,
+  IonGrid
+} from "@ionic/react";
+import { speedometerOutline } from "ionicons/icons";
 
 export const AddWeightEntry: FC = () => {
-  return <h1>AddWeightEntry</h1>;
+  return (
+    <>
+      <IonGrid>
+        <IonRow className="ion-justify-content-center">
+          <IonChip color="primary" outline className="ion-float-right">
+            <IonIcon icon={speedometerOutline} color="primary" />
+            <IonLabel>KG</IonLabel>
+          </IonChip>
+        </IonRow>
+      </IonGrid>
+
+      <IonItem>
+        <IonLabel position="stacked">
+          <IonText>Enter Weight</IonText>
+        </IonLabel>
+        <IonInput autofocus required inputMode="decimal" type="number" />
+      </IonItem>
+
+      <IonItem>
+        <IonLabel position="stacked">
+          <IonText>Date</IonText>
+        </IonLabel>
+        <IonDatetime
+          displayFormat="DD MMMM YYYY"
+          placeholder="Select Date"
+        ></IonDatetime>
+      </IonItem>
+    </>
+  );
 };
