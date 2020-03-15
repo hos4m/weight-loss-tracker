7af86e41f8c9f8ddbd31943e155bf48e1e567606
Hossam Mourad <ihos4m@gmail.com>
2020-03-15 02:31:48 -0400

    Display photos grouped by date

--- src/components/PhotosList.tsx
+++ src/components/PhotosList.tsx
@@ -1,17 +1,28 @@
 import React, { FC, Fragment } from "react";
+import { IonChip, IonIcon, IonLabel } from "@ionic/react";
+import { calendarOutline } from "ionicons/icons";
 
-import { PhotoEntry } from "../data/photos/types";
+import { PhotoEntriesGroupedByDateType } from "../data/photos/types";
 
 interface Props {
-  photos: PhotoEntry[];
+  photos: PhotoEntriesGroupedByDateType;
 }
 
 export const PhotosList: FC<Props> = ({ photos }) => {
   const renderPhotos = () => {
-    return photos.map(photo => {
+    return Object.keys(photos).map(date => {
       return (
-        <Fragment key={photo.date}>
-          <img src={photo.base64} alt={`Taken at ${photo.date}`} />
+        <Fragment key={date}>
+          <IonChip color="primary">
+            <IonIcon icon={calendarOutline} />
+            <IonLabel>{date}</IonLabel>
+          </IonChip>
+          {photos[date].map(photo => (
+            <Fragment key={photo.date}>
+              <img src={photo.base64} alt={`Taken at ${photo.date}`} />
+            </Fragment>
+          ))}
+          <hr />
         </Fragment>
       );
     });
